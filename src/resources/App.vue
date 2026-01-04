359a71b11d3e0e76e3ba2df12f6cbf361caa3d67
Manuel Alzate <manuel.alzate@hostinger.com>
2026-01-04 14:42:39 -0500

    fix: fix infinte loading in login

--- src/resources/App.vue
+++ src/resources/App.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app>
-    <MenuLayout>
+    <MenuLayout v-if="userStore.isLoggedIn">
       <router-view />
     </MenuLayout>
+    <router-view v-else />
   </v-app>
 </template>
 
 <script setup>
 import MenuLayout from '@/layouts/MenuLayout.vue'
+import { useUserStore } from '@/stores/useUserStore'
+
+const userStore = useUserStore()
 </script>
